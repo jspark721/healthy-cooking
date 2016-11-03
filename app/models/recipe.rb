f72cc9b94c1de309d1559b758bdb48fcb410db79
Julie Park <jspark721@gmail.com>
2016-11-03 13:12:05 -0700

    Added ingredient and directions model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,15 @@
 class Recipe < ActiveRecord::Base
+
+  has_many :ingredients, :directions
+  validates :title, :description, :image, presence: true
+  
   has_attached_file :image, styles: { medium: "400x400#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  accepts_nested_attributes_for :ingredients,
+                                reject_if: proc { |attributes| attributes['name'].blank? },
+                                allow_destroy: true
+  accepts_nested_attributes_for :directions,
+                                reject_if: proc { |attributes| attributes['step'].blank? },
+                                allow_destroy: true
 end
